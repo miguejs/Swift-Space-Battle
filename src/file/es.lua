82ddeaf455336ef8dcdc420e1e8e48b673001e84
Mike <imiked.mail@gmail.com>
2017-10-10 23:30:19 -0500

    Add es file

---
+++ src/file/es.lua
@@ -0,0 +1,23 @@
+language["es"] = {
+  name = "ESPAÑOL",
+
+  menu = {
+      play = "JUGAR",
+      score = "PUNTUACIÓN MAS ALTA",
+      options = "OPCIONES",
+      about = "ACERCA DE",
+      quit = "SALIR",
+      back = "REGRESAR",
+      resolution = "RESOLUCIÓN",
+      language = "IDIOMA",
+      restart = "REINICIAR PARA APLICAR CAMBIOS"
+  },
+
+  game = {
+      gameOver = "FIN DEL JUEGO",
+      record = "NUEVO RECORD:",
+      again = "¿JUGAR OTRA VEZ?",
+      yesAnswer = "SI",
+      noAnswer = "NO"
+  }
+}
